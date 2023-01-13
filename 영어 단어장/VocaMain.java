c13392bcdb91bb61c5bb79f13c1d6d332afb7f74
jh1004ben <88893644+wngud1004@users.noreply.github.com>
2023-01-13 22:00:14 +0900

    Create VocaMain.java
    영어 단어장의 main부분

---
+++ 영어 단어장/VocaMain.java
@@ -0,0 +1,24 @@
+package Vocaburay;
+
+import java.util.Scanner;
+
+public class VocaMain {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Vocaburary voca = new Vocaburary(("C:\\javajava\\Vocaburay\\voca1800.txt"));
+        voca.load(); //파일을 읽고 word객체를 생성해 해쉬맵에 저장
+
+        while (true) {
+            System.out.println("1.Find, 2.Add, 3.List, 4.Delete, 0.Exit >>>");
+            int menu = in.nextInt();
+            if (menu == 0) break;
+            switch (menu) {
+                case 1 -> voca.find();
+                case 2 -> voca.addWord();
+                case 3 -> voca.list();
+                case 4 -> voca.delete();
+            }
+        }
+    }
+}
